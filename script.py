bf6bb05df9bf720ce5fbdd83fb3ef744652521bf
Ivan Posca Doria <ivanpdoria@gmail.com>
2019-04-24 22:39:19 -0300

    Basic implementation of nodes and priority list

--- script.py
+++ script.py
@@ -1,13 +1,27 @@
 class Node:
-    def __init__(self, custo=0, vizinhos=None):
+    def __init__(self, nome="", custo=0, vizinhos=[]):
+        self.nome = nome
         self.custo = custo
-        self.filhos = vizinhos
+        self.vizinhos = vizinhos
 
     def print(self):
         print(self.custo)
 
 
-Nodo1 = Node(3)
-Nodo1.print()
-Nodo2 = Node(5)
-Nodo2.print()
+NodoA = Node("A", 5)
+NodoC = Node("C", 5)
+NodoG = Node("G", 5)
+NodoL = Node("L", 5)
+NodoO = Node("O", 5)
+NodoP = Node("P", 5)
+NodoF = Node("F", 5)
+NodoB = Node("B", 5)
+NodoM = Node("M", 5)
+NodoN = Node("N", 5)
+
+prioridades = ["A", "C", "G", "L", "O", "P", "F", "B", "M", "N"]
+
+print(NodoA.vizinhos)
+NodoA.vizinhos.append(NodoC)
+NodoA.vizinhos.append(NodoB)
+print(NodoA.vizinhos[0].nome)
