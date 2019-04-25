9aedae7ee7bf5e7f432754cf48e7eb208d956fad
Ivan Posca Doria <ivanpdoria@gmail.com>
2019-04-24 22:44:07 -0300

    Fix cost values

--- script.py
+++ script.py
@@ -9,15 +9,15 @@ class Node:
 
 
 NodoA = Node("A", 5)
-NodoC = Node("C", 5)
-NodoG = Node("G", 5)
-NodoL = Node("L", 5)
-NodoO = Node("O", 5)
-NodoP = Node("P", 5)
-NodoF = Node("F", 5)
+NodoC = Node("C", 10)
+NodoG = Node("G", 20)
+NodoL = Node("L", 13)
+NodoO = Node("O", 40)
+NodoP = Node("P", 10)
+NodoF = Node("F", 50)
 NodoB = Node("B", 5)
-NodoM = Node("M", 5)
-NodoN = Node("N", 5)
+NodoM = Node("M", 13)
+NodoN = Node("N", 13)
 
 prioridades = ["A", "C", "G", "L", "O", "P", "F", "B", "M", "N"]
 
@@ -25,3 +25,4 @@ print(NodoA.vizinhos)
 NodoA.vizinhos.append(NodoC)
 NodoA.vizinhos.append(NodoB)
 print(NodoA.vizinhos[0].nome)
+print(NodoA.vizinhos[0].custo)
