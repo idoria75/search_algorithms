9674d032082cdc24b8ee88378c8fe388c44c6258
Ivan Posca Doria <ivanpdoria@gmail.com>
2019-05-07 11:35:38 -0300

    Reason why energizar needs list of nodes

--- solver.py
+++ solver.py
@@ -2,25 +2,33 @@ from definicoes import *
 
 custoAtual = 0
 custoMaximo = 70
-vizinhosDisponiveis = [NodoA]
+vizinhosDisponiveis = ['A']
 energizados = []
 
 resultadoBusca = []
 custoBusca = 0
 
+# Lista: Lista dos vizinhos disponiveis
+# Resultado: Variavel que ira guardar o caminho ate o nodo
+# Custo: Variavel que ira guardar custo ate o nodo
+# Meta: Nodo meta
+# Lista deve ser lista de strings e nao de nodos para funcionar
+
 
 def depth_search(lista, resultado, custo, meta):
     print("Lista:" + str(lista))
+    # print(lista[0].getNome())
     print("Resultado:" + str(resultado))
     print("Custo:" + str(custo))
     print("Meta:" + str(meta))
     for i in lista:
-        print(i.getNome())
+        # print(i.getNome())
         # if i.getNome() == meta.getNome():
-        if(meta in lista):
+        # if(meta in lista):
+        if(i == meta.getNome()):
             return True
         else:
-            print("Nodo: "+i.getNome())
+            print("Nodo: "+i)
             print("Vizinhos: "+str(i.getNomeVizinhos()))
             print(energizar(i, custo, lista, i.getVizinhos())[1])
     return False
