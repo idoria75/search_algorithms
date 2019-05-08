e84dfca9d088f6c3733dcde66dce7025fca8c79f
Ivan Posca Doria <ivanpdoria@gmail.com>
2019-05-08 01:05:56 -0300

    Brainstorming notes

--- run.py
+++ run.py
@@ -1,1 +1,7 @@
 from solver import *
+
+# Salvar solucao atual
+# Varrer lista de nodos ate achar um nodo que atenda a solucao
+# Energizar este nodo e atualizar a situacao atual
+# Colocar uma flag no nodo pra indicar se ele ja esta energizado
+# Usar loop for pra verificar se todos os nodos do caminho antigo e do caminho atual foram energizados
